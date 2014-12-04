1edc10389d8664c5be5f15a688d8ec6af284a3f0
Malcolm Mathew <malcolmmmathew@gmail.com>
2014-12-03 20:20:12 -0500

    Setup with models

---
+++ december/polls/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+	question_text = models.CharField(max_length=250)
+	pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question)
+	choice_text = models.CharField(max_length=250)
+	votes = models.IntegerField(default=0)
+
+
